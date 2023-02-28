5b41847051374fa4fee7167a1b589590728a0fb4
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-28 14:43:10 +0530

    Add files via upload

---
+++ src/main/java/org/example/Graphclone.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+class Graphclone implements Cloneable
+{
+    Integer x;
+    Integer y;
+    Logger l = Logger.getLogger("kawin");
+    Graphclone(Integer x,Integer y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    boolean equals()
+    {
+        return x.equals(y);
+    }
+
+    Graphclone gp()
+    {
+        try
+        {
+            return (Graphclone) super.clone();
+        }
+        catch (CloneNotSupportedException e)
+        {
+            l.info("cloned error");
+            return this;
+        }
+    }
+}
